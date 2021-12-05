32970f51f6e6703347837b8625cb460b9d3580a8
Rafał Mikołajun <root@rmweb.pl>
2021-12-05 18:31:42 +0100

    add IsGranted to controller

--- src/Controller/Admin/IndexController.php
+++ src/Controller/Admin/IndexController.php
@@ -5,11 +5,13 @@
 
 namespace App\Controller\Admin;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends AbstractController
 {
+    #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig');
--- src/Controller/Site/IndexController.php
+++ src/Controller/Site/IndexController.php
@@ -5,11 +5,13 @@
 
 namespace App\Controller\Site;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends AbstractController
 {
+    #[IsGranted('ROLE_CLIENT')]
     public function index(): Response
     {
         return $this->render('site/index.html.twig');
